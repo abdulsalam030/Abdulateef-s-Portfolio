df288c8eeef95d1b8a312ca4b0265d5d708a5688
Abdulateef Abdulsalam <abdulsalamopeyemi030@gmail.com>
2024-01-26 15:30:47 +0100

    hero sectio edited

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -1,16 +1,10 @@
-import { HeroSectionType } from '@/lib/types/sections'
-import { resumeFileName } from '@/lib/utils/config'
+import { HeroSectionType } from '@/lib/types/sections';
 
 export const heroSection: HeroSectionType = {
-    subtitle: " Hello I'm ",
-    title: 'Abdulsalam Abdulateef.',
-    tagline: 'I create visually pleasing interfaces for the web.',
-    description:
+  subtitle: " Hello I'm ",
+  title: 'Abdulsalam Abdulateef.',
+  tagline: 'I create visually pleasing interfaces for the web.',
+  description:
     "A FrontEnd Developer with passion for creating neat and easy to use Web Applications. My expertise lies in the artistry of HTML, CSS, and JavaScript, where I weave responsive interfaces that captivate on every device. Let's bring your ideas to life with style and functionality! 🚀",
-    specialText: 'Currently available for a job & freelance',
-    cta: {
-        title: 'see my resume',
-        url: `/${resumeFileName}`,
-        hideInDesktop: true,
-    },
+  specialText: 'Currently available for a job & freelance',
 };
